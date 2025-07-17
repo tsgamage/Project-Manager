ccbb3fc100d19e32ba787f5dc7b59a443ee7f509
tsgamage <thevidu2007627@gmail.com>
2025-07-17 08:59:50 +0530

    feat: implement project CRUD operations; add CORS support

--- backend/controller/project.controller.js
+++ backend/controller/project.controller.js
@@ -2,100 +2,121 @@ import Project from "../models/projects.model.js";
 import mongoose from "mongoose";
 
 export const getAllProjects = async (req, res) => {
-    try {
-        const project = await Project.find({});
-        res.status(200).json({success: true, data: project});
-    } catch (e) {
-        console.log("cannot get project", e.message);
-        res.status(500).json({success: false, message: e.message});
-    }
-
-}
+  try {
+    const project = await Project.find({});
+    res.status(200).json({ success: true, data: project });
+  } catch (e) {
+    console.log("cannot get project", e.message);
+    res.status(500).json({ success: false, message: e.message });
+  }
+};
 
 export const getProjectById = async (req, res) => {
-    const {projectID} = req.params;
+  const { projectID } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(projectID)) {
-        return res.status(400).json({success: false, message: "Invalid projectID"});
+  if (!mongoose.Types.ObjectId.isValid(projectID)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid projectID" });
+  }
+
+  try {
+    const projects = await Project.findById(projectID);
+
+    if (!projects) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Project not found" });
+    } else {
+      return res
+        .status(200)
+        .json({ success: true, data: { projects: projects } });
     }
-
-    try {
-        const project = await Project.findById(projectID)
-
-        if (!project) {
-            return res.status(404).json({success: false, message: "Project not found"});
-        } else {
-            return res.status(200).json({success: true, data: project});
-        }
-
-    } catch (e) {
-        console.log(`cannot get project: ${projectID}`, e.message);
-        return res.status(500).json({success: false, message: e.message});
-    }
-}
+  } catch (e) {
+    console.log(`cannot get project: ${projectID}`, e.message);
+    return res.status(500).json({ success: false, message: e.message });
+  }
+};
 
 export const newProject = async (req, res) => {
-    const projectData = req.body;
+  const projectData = req.body;
 
-    if (
-        !projectData.title ||
-        !projectData.description ||
-        !projectData.startDate ||
-        !projectData.endDate ||
-        !projectData.status
-    ) {
-        return res
-            .status(400)
-            .json({success: false, message: "All fields are required"});
-    }
+  if (
+    !projectData.title ||
+    !projectData.description ||
+    !projectData.startDate ||
+    !projectData.endDate ||
+    !projectData.status
+  ) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
 
-    try {
-        const newProject = Project(projectData)
-        await newProject.save()
+  try {
+    const newProject = Project(projectData);
+    await newProject.save();
 
-        return res.status(201).json({
-            success: true,
-            message: "Created project",
-            data: newProject
-        })
-    } catch {
-        return res.status(500).json({success: false, message: "Something went wrong"});
-    }
-
-}
+    return res.status(201).json({
+      success: true,
+      message: "Created project",
+      data: newProject,
+    });
+  } catch {
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong" });
+  }
+};
 
 export const updateProject = async (req, res) => {
-    const {projectID} = req.params
-    const projectData = req.body;
+  const { projectID } = req.params;
+  const projectData = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(projectID)) {
-        return res.status(400).json({success: false, message: "Invalid projectID"});
-    }
+  if (!mongoose.Types.ObjectId.isValid(projectID)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid projectID" });
+  }
 
-    try {
-        const updatedProject = await Project.findByIdAndUpdate(projectID, projectData, {new: true});
-        return res.status(200).json({success: true, data: updatedProject});
-    } catch (e) {
-        return res.status(500).json({success: false, message: e.message});
-    }
-}
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(
+      projectID,
+      projectData,
+      { new: true }
+    );
+    return res.status(200).json({ success: true, data: updatedProject });
+  } catch (e) {
+    return res.status(500).json({ success: false, message: e.message });
+  }
+};
 
 export const deleteProject = async (req, res) => {
-    const {projectID} = req.params
+  const { projectID } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(projectID)) {
-        return res.status(400).json({success: false, message: "Invalid projectID"});
+  if (!mongoose.Types.ObjectId.isValid(projectID)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid projectID" });
+  }
+
+  try {
+    const deletedProject = await Project.findByIdAndDelete(projectID);
+
+    if (!deletedProject) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Project not found" });
     }
 
-    try {
-        const deletedProject = await Project.findByIdAndDelete(projectID)
-
-        if(!deletedProject) {
-            return res.status(404).json({success: false, message: "Project not found"});
-        }
-
-        return res.status(200).json({success: true, message: "Successfully deleted", data: deletedProject});
-    } catch (e) {
-        return res.status(500).json({success: false, message: e.message});
-    }
-}
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Successfully deleted",
+        data: deletedProject,
+      });
+  } catch (e) {
+    return res.status(500).json({ success: false, message: e.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectionDB} from "./config/db.js";
+import { connectionDB } from "./config/db.js";
 import projectRoute from "./routes/project.route.js";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
-app.use(express.json())
-dotenv.config();
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/project", projectRoute)
+app.use("/api/project", projectRoute);
 
 app.listen(PORT, "0.0.0.0", async () => {
-    console.log("Server is running on port", PORT);
-    await connectionDB();
+  console.log("Server is running on port", PORT);
+  await connectionDB();
 });
