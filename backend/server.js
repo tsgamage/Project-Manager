d2a6f1f86a5d7c44d2f11bca5b68f66c10804fff
tsgamage <thevidu2007627@gmail.com>
2025-07-27 14:04:15 +0530

    fix: reorder CORS middleware initialization
    - Moved CORS middleware placement to ensure correct middleware execution order.

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
