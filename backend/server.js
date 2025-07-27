94ad8e486cfad6cbbfa978e10612c69d15ba2b29
tsgamage <thevidu2007627@gmail.com>
2025-07-27 10:15:48 +0530

    feat: add authentication routes and improve server setup
    - Integrated `authRoutes` for authentication-related API endpoints.
    - Updated CORS configuration to include `credentials` for secure cross-origin requests.
    - Renamed `projectRoute` to `projectRoutes` for consistency.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectionDB } from "./config/db.js";
-import projectRoute from "./routes/project.route.js";
 import cors from "cors";
+import projectRoutes from "./routes/project.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/project", projectRoute);
+app.use("/api/project", projectRoutes);
+app.use("/api/auth/", authRoutes);
 
 app.listen(PORT, "0.0.0.0", async () => {
   console.log("Server is running on port", PORT);
