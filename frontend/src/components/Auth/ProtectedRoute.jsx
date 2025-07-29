e09b233d66cd780e2a8a5a4dbd8248a2be4ef5c3
tsgamage <thevidu2007627@gmail.com>
2025-07-29 06:45:09 +0530

    feat(auth): add `viewing` prop to ProtectedRoute for enhanced navigation control
    - Introduced `viewing` prop to conditionally manage redirects.
    - Improved route behavior for authenticated and verified users accessing specific views.

--- frontend/src/components/Auth/ProtectedRoute.jsx
+++ frontend/src/components/Auth/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react";
 import AuthContext from "../../store/auth.context.jsx";
 import { useNavigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute({ children, viewing }) {
   const { isAuthenticated, user, isCheckingAuth } = useContext(AuthContext); // Assuming AuthContext provides authentication state
   const navigate = useNavigate();
 
@@ -11,10 +11,10 @@ export default function ProtectedRoute({ children }) {
       navigate("/auth/login", { replace: true });
     } else if (!isCheckingAuth && user && user.isVerified === false) {
       navigate("/auth/verify-mail", { replace: true });
-    } else if (!isCheckingAuth && user && user.isVerified === true) {
+    } else if (!isCheckingAuth && user && user.isVerified === true && viewing === "auth") {
       navigate("/", { replace: true });
     }
-  }, [isCheckingAuth, isAuthenticated, user, navigate]);
+  }, [isCheckingAuth, isAuthenticated, user, navigate, viewing]);
 
   if (!isAuthenticated) {
     return null;
