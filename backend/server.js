6b23d9ebaddb6e251ccc5a8ce7cb5d206feab600
tsgamage <thevidu2007627@gmail.com>
2025-07-29 15:00:42 +0530

    feat(build, backend): add production build and setup static file serving
    - Added new scripts to `package.json` for building (`build`), development (`dev`), and production (`start`) environments.
    - Updated `server.js` to serve static files from `frontend/dist` in production.

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ import projectRoutes from "./routes/project.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
+import path from "path";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -19,6 +21,13 @@ app.use(cookieParser());
 app.use("/api/project", projectRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, async () => {
   await connectionDB();
